ec2d7e934d359e71916a478560184cdcbfa1e7d0
webawsess <168604101+webawsess@users.noreply.github.com>
2024-04-30 17:02:17 -0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+  images: {
+    domains: ['altlayer.io'],
+  },
+};
+
+module.exports = nextConfig;
+
